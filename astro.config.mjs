f070d0457d007d09601d815b620fed4a9620bbde
Thathsara <thathsaramadhhusha@gmail.com>
2026-04-07 22:06:43 +0530

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ilandarideva.com"
+  site: "https://ilandarideva.com",
+  devToolbar: {
+    enabled: false,
+  },
 });
